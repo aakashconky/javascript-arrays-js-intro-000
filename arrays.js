b94857e1f6cbaa8bdc661da73478797189cd4f22
aakash thapa <aakash_conky@hotmail.com>
2020-12-05 23:10:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,17 @@
 var chocolateBars = [snickers, hundredgrand, kitkat, skittles];
 
 function addElementToEndOfArray (array, element){
-return [...array, element]
+return [element,...array]
 }
 function destructivelyAddElementToEndOfArray (array, element) {
   array.unshift(element)
+return array
+}
+
+function addElementToEndOfArray (array, element){
+return [array, element...]
+}
+function destructivelyAddElementToEndOfArray (array, element) {
+  array.element
 return array
 }
